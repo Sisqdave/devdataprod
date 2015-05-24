3cc611d1108c28806d8ce327d39c53c3c4b32b44
David Moody <sisqdave@gmail.com>
2015-05-24 10:22:32 -0700

    UI creation
    Added four input sliders

--- ui.R
+++ ui.R
@@ -1,32 +1,21 @@
 library(shiny)
-# diabetesRisk <- function(glucose) glucose/200
-# shinyUI(pageWithSidebar(
-#         headerPanel("Illustraing inputs"),
-#         sidebarPanel(
-#                 numericInput('id1', 'Numeric input, labeled id1', 0, min = 0, max = 10, step = 1),
-#         checkboxGroupInput("id2", "Checkbox", 
-#                            c("Value 1" = "1",
-#                              "Value 2" = "2",
-#                              "Value 3" = "3")),
-#         dateInput("date", "Date:")
-#         ),
-#         mainPanel(
-#                 h3('Illustrating Outputs'),
-#                 h4('You entered'),
-#                 verbatimTextOutput("oid1"),
-#                 h4('You entered'),
-#                 verbatimTextOutput("oid2"),
-#                 h4('You entered'),
-#                 verbatimTextOutput("odate")
-#         )
-# ))
 
-shinyUI(pageWithSidebar(
-        headerPanel("Example plot"),
-        sidebarPanel(
-                sliderInput('mu', 'Guess at the mu',value = 70, min = 60, max = 80, step = 0.05,)
+
+shinyUI(fluidPage(
+        titlePanel("Iris classification using petal and sepal size"),
+        
+        sidebarLayout(
+            sidebarPanel(
+                    helpText("Select petal and sepal measurements"),
+                   
+                    sliderInput('SL', 'Sepal Length',  value = 4, min = 4, max = 8, step = .1),
+                    sliderInput('SW', 'Sepal Width',  value = 2, min = 2, max = 4.5, step = .1),
+                    sliderInput('PL', 'Petal Length',  value = 1, min = 1, max = 7, step = .1),
+                    sliderInput('PW', 'Petal Width', value = 0.1, min = 0.1, max = 2.5, step = .1)
         ),
+        
         mainPanel(
                 plotOutput('myHist')
+        )
         )
 ))
