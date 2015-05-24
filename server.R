f1dc9d25fd0db78766745089f571cebe5f70e94e
David Moody <sisqdave@gmail.com>
2015-05-24 12:51:09 -0700

    working 2 slider

--- server.R
+++ server.R
@@ -15,14 +15,12 @@ shinyServer(
                 modlda <- train(Species ~ ., data = iris, method="lda")
                 modelAccuracy <- round(modlda$results$Accuracy, 3)
                 answer <- reactive({
-                         modlda <- train(Species ~ ., data = iris, method="lda") 
-                         sample <- data.frame(input$SL, input$SW, input$PL, input$PW)
-                         names(sample) <- names(iris)[1:4]
+                         modlda <- train(Species ~ Petal.Width + Petal.Length, data = iris, method="lda") 
+                         sample <- data.frame(input$PL, input$PW)
+                         names(sample) <- names(iris)[3:4]
                          as.character(predict(modlda, sample ))
                 })
                 testText <- "Hello"
-                output$text1 <- renderText({paste("The Sepal Length sample measurement is",input$SL, "cm")})
-                output$text2 <- renderText({paste("The Sepal Width sample measurement is",input$SW, "cm")})
                 output$text3 <- renderText({paste("The Petal Length sample measurement is",input$PL, "cm")})
                 output$text4 <- renderText({paste("The Petal Width sample measurement is",input$PW, "cm")})
                 output$modelAccuracy <- renderText({paste("The accuracy of this model is ", modelAccuracy)})
--- ui.R
+++ ui.R
@@ -8,15 +8,11 @@ shinyUI(fluidPage(
                     helpText("Use the sliders to select petal and sepal measurements(in centimeters) to classify the type of iris
                              based on an LDA model"),
                    
-                    sliderInput('SL', 'Sepal Length',  value = 4, min = 4, max = 8, step = .1),
-                    sliderInput('SW', 'Sepal Width',  value = 2, min = 2, max = 4.5, step = .1),
                     sliderInput('PL', 'Petal Length',  value = 1, min = 1, max = 7, step = .1),
                     sliderInput('PW', 'Petal Width', value = 0.1, min = 0.1, max = 2.5, step = .1)
         ),
         
         mainPanel(
-                textOutput("text1"),
-                textOutput("text2"),
                 textOutput("text3"),
                 textOutput("text4"),
                 textOutput("modelAccuracy"),
