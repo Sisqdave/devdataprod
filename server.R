6adbbcdd692eb5164ec8b364b70ddb9ba55f4ffb
David Moody <sisqdave@gmail.com>
2015-05-24 14:02:16 -0700

    Close to final submission

--- server.R
+++ server.R
@@ -1,16 +1,10 @@
 library(shiny)
-# shinyServer(
-#         function(input,output) {
-#                 output$oid1 <- renderPrint({input$id1})
-#                 output$oid2 <- renderPrint({input$id2})
-#                 output$odate <- renderPrint({input$date})
-#         }
-# )
+
 library(caret)
 data(iris)
 library(e1071)
-#modlda <- train(Species ~ ., data = iris, method="lda")
-#modelAccuracy <- round(modlda$results$Accuracy, 3)
+modlda <- train(Species ~ ., data = iris, method="lda")
+modelAccuracy <- round(modlda$results$Accuracy, 3)
 shinyServer(
         function(input, output) {
                 
@@ -24,7 +18,7 @@ shinyServer(
 
                 output$text3 <- renderText({paste("The Petal Length sample measurement is",input$PL, "cm")})
                 output$text4 <- renderText({paste("The Petal Width sample measurement is",input$PW, "cm")})
-#                output$modelAccuracy <- renderText({paste("The accuracy of this model is ", modelAccuracy)})
+                output$modelAccuracy <- renderText({paste("The accuracy of this model is ", modelAccuracy)})
                 output$answer <- renderText({paste("The Species is most likely ", answer())})
         }
 )
--- ui.R
+++ ui.R
@@ -1,7 +1,7 @@
 library(shiny)
 
 shinyUI(fluidPage(
-        titlePanel("Iris classification using petal and sepal size"),
+        titlePanel("Iris classification using petal width and length measurements"),
         
         sidebarLayout(
             sidebarPanel(
@@ -13,8 +13,14 @@ shinyUI(fluidPage(
         ),
         
         mainPanel(
+                h3("Using the slider bars in the left column, select the petal width and length of the Iris petal sample"),
+                h5("Here are some examples you can try:"), 
+                h5("Setosa- width 0.5cm, length 1.5cm"),
+                h5("Versicolor - width 1.2cm, length 4.0cm"),
+                h5("Virginica - width 2.5cm, length 6.0cm"),
                 textOutput("text3"),
                 textOutput("text4"),
+                h5("Using a LDA model on the famous (Fisher's or Anderson's) iris data set gives the measurements in centimeters of the variables sepal length and width and petal length and width, respectively, for 50 flowers from each of 3 species of iris. The species are Iris setosa, versicolor, and virginica."),
                 textOutput("modelAccuracy"),
                 textOutput("answer")
         )
